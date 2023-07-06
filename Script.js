9ef50de58a05a18b4cb889234d6c6d72f62130fe
Maria Vitória <133173885+vitoriamso@users.noreply.github.com>
2023-07-06 22:26:55 +0000

    Atualizando a arquivo script.js para os comandos da atividade Hora do dia

--- Script.js
+++ Script.js
@@ -1,1 +1,17 @@
-alert("tente novamente")
+function carregar(){
+    var msg= window.document.getElementById('msg')
+    var img= window.document.getElementById('imagem')
+    var data= new Date()
+    var hora= data.getHours()
+    msg.innerHTML = 'Agora são ${hora} horas'
+    if(hora >=0 && hora < 12){
+        // Bom Dia
+        img.src= 'fotomanha.png'
+    }else if (hora >= 12 && < 18){
+        //Boa tarde
+        img.src= 'fototarde.png'
+    }else{
+        //boa noite
+        img.src= 'fotonoite.png'
+    }
+}

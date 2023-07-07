0c90b4177df41326dac4beac9821d71c93517e1f
Maria Vitória <133173885+vitoriamso@users.noreply.github.com>
2023-07-07 16:57:03 +0000

    finalizações

--- Script.js
+++ Script.js
@@ -28,11 +28,13 @@ function verificar () {
     if (fano.value.length == 0 || Number(fano.value) > ano ) {
         window.alert('[ERRO] verifique os dados e tente novamente')
     } else {
+
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)       
         var genero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
+        
        if(fsex[0].checked){
             genero = 'homem'
             if(idade >= 0 && idade < 12){

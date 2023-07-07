9405fab1dfe4f90de1e0ca5ba226303d579f4405
Maria Vitória <133173885+vitoriamso@users.noreply.github.com>
2023-07-07 16:34:44 +0000

    resolvendo a atividade de idade

--- Script.js
+++ Script.js
@@ -18,3 +18,51 @@ function carregar(){
         document.body.style.background = '#08191f'
     }
 }
+
+function verificar () {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano ) {
+        window.alert('[ERRO] verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)       
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+       if(fsex[0].checked){
+            genero = 'homem'
+            if(idade >= 0 && idade < 12){
+                img.setAttribute('src', 'crianco.png')
+            }else if(idade < 21) {
+                img.setAttribute('src', 'hadolescente.png')
+                
+            }else if(idade < 60) {
+                img.setAttribute('src', 'adulto.png')
+            }else {
+                img.setAttribute('src', 'idoso.png')
+            }            
+            
+        }else if (fsex[1].checked){
+            genero = 'mulher'
+            if(idade >=0 && idade < 12){
+                img.setAttribute('src', 'crianca.png')
+            }else if(idade < 21){
+                img.setAttribute('src', 'madolescente.png')                
+            }else if(idade < 60){
+                img.setAttribute('src', 'adulta.png')
+                
+            }else{
+                img.setAttribute('src', 'idosa.png')
+            }       
+            
+        }
+        res.style.textAlign= 'center'
+        res.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+   
+}

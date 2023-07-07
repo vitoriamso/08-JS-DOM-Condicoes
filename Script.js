c35b009daab36bad88ee03bffb84213cdec41f72
Maria Vitória <133173885+vitoriamso@users.noreply.github.com>
2023-07-07 15:06:11 +0000

    melhorias

--- Script.js
+++ Script.js
@@ -3,18 +3,18 @@ function carregar(){
     var img= window.document.getElementById('img');
     var data= new Date();
     var hora= data.getHours();
-    msg.innerHTML = 'Agora são ${hora} horas';
+    msg.innerHTML = `Agora são ${hora} horas`;
     if(hora >=0 && hora < 12){
         // Bom Dia
-        img.src= "./imgHoradodia/fotomanha.png";
-        document.body.style.background = "#4273c2";
+        img.src= 'fotomanha.png';
+        document.body.style.background = '#4273c2';
     }else if (hora >= 12 && hora <= 18){
         //Boa tarde
-        img.src= "./imgHoradodia/fototarde.png";
-        document.body.style.background = "#c27e2b";
+        img.src= 'fototarde.png';
+        document.body.style.background = '#c27e2b';
     }else{
         //boa noite
-        img.src= img.src = "./imgHoradodia/fotonoite.png";
-        document.body.style.background = "#08191f";
+        img.src= img.src = 'fotonoite.png';
+        document.body.style.background = '#08191f';
     }
 }
